1736471e7b23aabe3119e846cdc6cb60eb3217e5
tdiaz1 <tj.diaz@uniandes.edu.co>
2021-06-04 22:19:51 -0500

    se agrega consumo de servicio

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-import {PaymentStorage} from '../../models/paymentStorage';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-payment',
